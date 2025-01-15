147bc83a515dee1f3a2fcdead227e275bbd33a5c
AbbyWahyuSadewa <125106565+AbbyWahyuSadewa@users.noreply.github.com>
2025-01-15 15:03:48 +0700

    add new models and migrations

---
+++ database/migrations/2025_01_15_065348_create_cars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('plate')->nullable();
+            $table->enum('type', ['sedan', 'MPV', 'SUV'])->nullable();
+            $table->string('capacity')->nullable();
+            $table->string('price')->nullable();
+            $table->text('image')->nullable();
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
