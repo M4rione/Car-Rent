dbd62a2dbcf6f3c41dff72e0802f2f7f8d286978
AbbyWahyuSadewa <125106565+AbbyWahyuSadewa@users.noreply.github.com>
2025-01-17 13:34:52 +0700

    add register

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('register', [RegisterController::class, 'index'])->name('register');
